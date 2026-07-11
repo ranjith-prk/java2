0a854eb55e72e22a5941d94d725ea4a759cef7a8
ranjith-prk <ranjith.panchamurthi@gmail.com>
2026-07-11 08:59:49 +0530

    parser version name changed

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -13,7 +13,7 @@ import com.github.javaparser.utils.SourceRoot;
 import java.nio.file.Paths;
 
 /**
- * Some code that uses an JavaParser.
+ * Some code that uses JavaParser.
  */
 public class LogicPositivizer {
     public static void main(String[] args) {
